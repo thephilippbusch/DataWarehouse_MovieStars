bb3db8dcfaa69235957112fb9a14a12eb401cfcc
Philipp Busch <philipp-leon-busch@hotmail.de>
2021-02-04 16:58:17 +0100

    Clean up commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const Main = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit and save to reload.
         </p>
         <a
           className="App-link"
@@ -22,4 +20,4 @@ function App() {
   );
 }
 
-export default App;
+export default Main;
