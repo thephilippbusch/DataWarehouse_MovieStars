5dfa23436cbdad523db3e5223e358dd77810a5de
Philipp Busch <philipp-leon-busch@hotmail.de>
2021-02-04 17:24:01 +0100

    first api call

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import axios from "axios";
+
+const options = {
+  method: 'GET',
+  url: 'https://movie-database-imdb-alternative.p.rapidapi.com/',
+  params: {i: 'tt4154796', r: 'json'},
+  headers: {
+    'x-rapidapi-key': '39eb07b247msh5a360614c8c1fa7p168871jsn8fc6c6c0765b',
+    'x-rapidapi-host': 'movie-database-imdb-alternative.p.rapidapi.com'
+  }
+};
 
 const Main = () => {
+  const handleDataFetch = () => {
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => handleDataFetch()}>Get Movie Data</button>
     </div>
   );
 }
